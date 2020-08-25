acc6b059008af235559570e6ba68af4b76295cb4
Pullusb <bernou.samuel@gmail.com>
2020-08-25 14:28:39 +0200

    added licence

--- __init__.py
+++ __init__.py
@@ -1,4 +1,15 @@
-# coding: utf-8
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
     "name": "GP clipboard",
@@ -599,6 +610,7 @@ class GPCLIP_PT_clipboard_ui(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Gpencil"
+    bl_options = {'DEFAULT_CLOSED'}
 
     def draw(self, context):
         layout = self.layout
@@ -648,7 +660,7 @@ def register_keymaps():
     km = addon.keymaps.new(name = "Grease Pencil", space_type = "EMPTY", region_type='WINDOW')# in Grease context
     # km = addon.keymaps.new(name = "3D View", space_type = "VIEW_3D")# in 3D context
     # km = addon.keymaps.new(name = "Window", space_type = "EMPTY")# from everywhere
-    
+
     kmi = km.keymap_items.new("gp.copy_strokes", type = "C", value = "PRESS", ctrl=True, shift=True)
     kmi.repeat = False
     kmi = km.keymap_items.new("gp.cut_strokes", type = "X", value = "PRESS", ctrl=True, shift=True)
